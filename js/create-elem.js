2668b1126e68bfd1830a11ae41216cdd6ed576da
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-04-29 22:47:21 +0300

    refactor: delete some spaces

--- js/create-elem.js
+++ js/create-elem.js
@@ -1,14 +1,14 @@
 export function createElem(name, classes, content) {
-    const element = document.createElement(name);
-    if (Array.isArray(classes)) {
-        classes.forEach(c => {
-            element.classList.add(c)
-        })
-    } else {
-        element.classList.add(classes)
-    }
+  const element = document.createElement(name);
+  if (Array.isArray(classes)) {
+      classes.forEach(c => {
+          element.classList.add(c)
+      })
+  } else {
+      element.classList.add(classes)
+  }
 
-    if (content) element.textContent = content;
+  if (content) element.textContent = content;
 
-    return element
+  return element
 }
